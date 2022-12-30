7906f2cd6d8511a9573819ef629da7f00e9f7764
brotherhodu <dwn920919@gmail.com>
2022-12-31 00:19:38 +0900

    20221231_dart

---
+++ Dart/coding_chief/variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Mary";
+  double pi = 3.14;
+  // camel case 방식의 변수명 지정
+  bool isLogin = false;
+
+  int sum = add();
+  print(sum);
+}
+
+int add() {
+  return 3 + 4;
+}
---
+++ Dart/coding_chief/variables_example.dart
@@ -0,0 +1,13 @@
+void main() {
+  int num = 77;
+  double gravity = 9.8;
+  bool isLogin = true;
+
+  int add() {
+    int first = 30;
+    int second = 20;
+    return first + second;
+  }
+
+  print(add());
+}
