e9cf8eae0285aacfa4c552bd44e3238938c450ac
brotherhodu <dwn920919@gmail.com>
2023-01-06 09:25:17 +0900

    20230106_dart

--- flutter/myapp/lib/main.dart
+++ flutter/myapp/lib/main.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+// 최상위 위젯인 MyApp은 정적인 위젯
+// Stateless 위젯으로 지정
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(
+          // 색상 견본
+          primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
