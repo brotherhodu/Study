3fc6932c1af72f8f5e5ade90fffe24b933bd1590
brotherhodu <dwn920919@gmail.com>
2023-01-10 09:04:57 +0900

    20230110_flutter_homerapp

--- flutter/widget_practice/lib/main.dart
+++ flutter/widget_practice/lib/main.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -12,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'BBANTO',
       home: Grade(), //Grade 커스텀 위젯 생성
     );
@@ -36,6 +35,18 @@ class Grade extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/homer.gif'),
+                radius: 100.0,
+              ),
+            ),
+            Divider(
+              height: 60.0, //divder 위와 아래 사이의 간격
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
             Text(
               'NAME',
               style: TextStyle(
@@ -55,6 +66,77 @@ class Grade extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'BBANTO POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '14',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: const [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('face hero tattoo',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    ))
+              ],
+            ),
+            Row(
+              children: const [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    ))
+              ],
+            ),
+            Row(
+              children: const [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    ))
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/homer.jpg'),
+                radius: 20.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            )
           ],
         ),
       ),
